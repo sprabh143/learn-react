c99b7ab3ec111dfed2820546dd47762c7d8c3d81
sprabh143 <38618144+sprabh143@users.noreply.github.com>
2024-12-12 07:41:06 +0530

    Update index.jsx

--- 01. Static pages/04. First React Challenge/index.jsx
+++ 01. Static pages/04. First React Challenge/index.jsx
@@ -3,3 +3,12 @@
  * This time, try rendering an unordered list with 2-3 list items inside
  * with why you're excited to be learning React.
  */
+import {    createRoot  } from 'react-dom/client'
+
+const root = createRoot (document.getElementById('root'))
+
+root.render(<ul>
+    <li>bobo1</li>
+    <li>bobo2</li>
+</ul>)
+
