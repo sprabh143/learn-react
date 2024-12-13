8146cbb39903282612c5594dfbc4b5eba57644d1
sprabh143 <38618144+sprabh143@users.noreply.github.com>
2024-12-13 15:14:48 +0530

    Update index.jsx

--- 01. Static pages/12. ReactFacts Project - Markup/index.jsx
+++ 01. Static pages/12. ReactFacts Project - Markup/index.jsx
@@ -12,3 +12,20 @@ Hints:
 * You can also set the `width` attribute of the image element
   just like in HTML. In the slide, I have it set to 40px
  */
+import { createRoot } from "react-dom/client"
+import { createElement } from "react"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+  <main class="">
+    <img src="react-logo.png" width="40px" alt="react logo"/>
+    <h1>bobo</h1>
+    <ul>
+      <li>bobo1</li>
+      <li>bobo2</li>
+      <li>bobo3</li>
+      <li>bobo4</li>
+    </ul>
+  </main>
+)
